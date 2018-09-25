b104fb1d330dec74878799029b0cef4e5d215cf6
Matthieu Bosquet <matthieubosquet@gmail.com>
2018-09-25 17:06:34 +0100

    Container for latest ukparliament graph running in GraphDB

---
+++ graphdb/getArtifact.ps1
@@ -0,0 +1,7 @@
+$storageAccountKeys = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName
+
+$storageAccountPrimaryKey = $storageAccountKeys[0].Value
+
+$azureStorageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountPrimaryKey
+
+Get-AzureStorageBlobContent -Context $azureStorageContext -Blob $blob -Container $container -Destination $blob
