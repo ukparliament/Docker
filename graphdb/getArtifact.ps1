563ef80d264d7a6d7f08699f4b2610525f1f177d
Matthieu Bosquet <matthieubosquet@gmail.com>
2018-09-25 17:35:48 +0100

    Try correctly passing parameters

--- graphdb/getArtifact.ps1
+++ graphdb/getArtifact.ps1
@@ -1,7 +1,41 @@
-$storageAccountKeys = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName
+<#
+.SYNOPSIS
+Gets artifact from blob storage.
 
-$storageAccountPrimaryKey = $storageAccountKeys[0].Value
+.DESCRIPTION
+Azure Powershell script to get artifacts from a blob storage in build and release process.
 
-$azureStorageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountPrimaryKey
+.PARAMETER ResourceGroupName 
+The resource group name of the azure storage account.
 
-Get-AzureStorageBlobContent -Context $azureStorageContext -Blob $blob -Container $container -Destination $destination/$blob
+.PARAMETER StorageAccountName
+The blob storage account name.
+
+.PARAMETER Container
+Name of the container in storage account.
+
+.PARAMETER Blob
+Name of the blob in container.
+
+.PARAMETER Destination
+Destination path of the blob file.
+
+.NOTES
+This script is for use as a part of deployment in VSTS only.
+#>
+
+Param(
+    [Parameter(Mandatory=$true)] [string] $ResourceGroupName,
+    [Parameter(Mandatory=$true)] [string] $StorageAccountName,
+    [Parameter(Mandatory=$true)] [string] $Container,
+    [Parameter(Mandatory=$true)] [string] $Blob,
+    [Parameter(Mandatory=$true)] [string] $Destination  
+)
+
+$StorageAccountKeys = Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
+
+$StorageAccountPrimaryKey = $StorageAccountKeys[0].Value
+
+$AzureStorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountPrimaryKey
+
+Get-AzureStorageBlobContent -Context $AzureStorageContext -Blob $Blob -Container $Container -Destination $Destination/$Blob
